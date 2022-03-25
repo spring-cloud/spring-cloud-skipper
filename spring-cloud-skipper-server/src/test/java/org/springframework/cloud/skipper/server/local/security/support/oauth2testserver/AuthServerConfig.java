f11d2c286cce30ef1c68c4e5e9c7bd657fe5629c
Corneil du Plessis <pcorneil@vmware.com>
2022-03-25 09:52:42 +0200

    Updated copyright dates. Added common function for removing CR from linefeed.
    Fixes #1036

--- spring-cloud-skipper-server/src/test/java/org/springframework/cloud/skipper/server/local/security/support/oauth2testserver/AuthServerConfig.java
+++ spring-cloud-skipper-server/src/test/java/org/springframework/cloud/skipper/server/local/security/support/oauth2testserver/AuthServerConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.cloud.skipper.server.local.security.support.oauth2testserver;
 
 import java.util.HashSet;
@@ -27,7 +28,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.builders.ClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -35,9 +35,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
@@ -50,95 +48,93 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private BaseClientDetails details;
+	@Autowired
+	private BaseClientDetails details;
 
-    private AuthenticationManager authenticationManagerBean;
+	private AuthenticationManager authenticationManagerBean;
 
-    @Autowired
-    public void setAuthenticationManagerBean(AuthenticationManager authenticationManagerBean) {
-        this.authenticationManagerBean = authenticationManagerBean;
-    }
+	@Autowired
+	public void setAuthenticationManagerBean(AuthenticationManager authenticationManagerBean) {
+		this.authenticationManagerBean = authenticationManagerBean;
+	}
 
-    @Override
-    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        oauthServer
-                .tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()");
-    }
+	@Override
+	public void configure(final AuthorizationServerSecurityConfigurer oauthServer) {
+		oauthServer
+				.tokenKeyAccess("permitAll()")
+				.checkTokenAccess("isAuthenticated()");
+	}
 
-    @Override
-    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
-        ClientDetailsServiceBuilder<InMemoryClientDetailsServiceBuilder>.ClientBuilder builder = clients
-                .inMemory().withClient(this.details.getClientId());
-        builder.secret(this.details.getClientSecret())
-                .resourceIds(this.details.getResourceIds().toArray(new String[0]))
-                .authorizedGrantTypes(
-                        this.details.getAuthorizedGrantTypes().toArray(new String[0]))
-                .authorities(AuthorityUtils
-                        .authorityListToSet(this.details.getAuthorities())
-                        .toArray(new String[0]))
-                .scopes(this.details.getScope().toArray(new String[0]));
+	@Override
+	public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+		ClientDetailsServiceBuilder<InMemoryClientDetailsServiceBuilder>.ClientBuilder builder = clients
+				.inMemory().withClient(this.details.getClientId());
+		builder.secret(this.details.getClientSecret())
+				.resourceIds(this.details.getResourceIds().toArray(new String[0]))
+				.authorizedGrantTypes(
+						this.details.getAuthorizedGrantTypes().toArray(new String[0]))
+				.authorities(AuthorityUtils
+						.authorityListToSet(this.details.getAuthorities())
+						.toArray(new String[0]))
+				.scopes(this.details.getScope().toArray(new String[0]));
 
-        if (this.details.getAutoApproveScopes() != null) {
-            builder.autoApprove(
-                    this.details.getAutoApproveScopes().toArray(new String[0]));
-        }
-        if (this.details.getAccessTokenValiditySeconds() != null) {
-            builder.accessTokenValiditySeconds(
-                    this.details.getAccessTokenValiditySeconds());
-        }
-        if (this.details.getRefreshTokenValiditySeconds() != null) {
-            builder.refreshTokenValiditySeconds(
-                    this.details.getRefreshTokenValiditySeconds());
-        }
-        if (this.details.getRegisteredRedirectUri() != null) {
-            builder.redirectUris(
-                    this.details.getRegisteredRedirectUri().toArray(new String[0]));
-        }
-    }
+		if (this.details.getAutoApproveScopes() != null) {
+			builder.autoApprove(
+					this.details.getAutoApproveScopes().toArray(new String[0]));
+		}
+		if (this.details.getAccessTokenValiditySeconds() != null) {
+			builder.accessTokenValiditySeconds(
+					this.details.getAccessTokenValiditySeconds());
+		}
+		if (this.details.getRefreshTokenValiditySeconds() != null) {
+			builder.refreshTokenValiditySeconds(
+					this.details.getRefreshTokenValiditySeconds());
+		}
+		if (this.details.getRegisteredRedirectUri() != null) {
+			builder.redirectUris(
+					this.details.getRegisteredRedirectUri().toArray(new String[0]));
+		}
+	}
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        super.configure(endpoints);
-        endpoints.authenticationManager(authenticationManagerBean);
-        endpoints.tokenStore(tokenStore());
-        endpoints.tokenEnhancer(new TokenEnhancer() {
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		super.configure(endpoints);
+		endpoints.authenticationManager(authenticationManagerBean);
+		endpoints.tokenStore(tokenStore());
+		endpoints.tokenEnhancer((accessToken, authentication) -> {
+			if (authentication.getPrincipal() instanceof User) {
+				final User user = (User) authentication.getPrincipal();
 
-            @Override
-            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-                if (authentication.getPrincipal() instanceof User) {
-                    final User user = (User) authentication.getPrincipal();
+				final Set<String> scopes = new HashSet<>();
+				for (GrantedAuthority authority : user.getAuthorities()) {
+					final String role = authority.getAuthority();
 
-                    final Set<String> scopes = new HashSet<String>();
-                    for (GrantedAuthority authority : user.getAuthorities()) {
-                        final String role = authority.getAuthority();
+					if (role.startsWith("ROLE_")) {
+						scopes.add(role.substring(5).toLowerCase());
+					}
+					else {
+						scopes.add(role.toLowerCase());
+					}
+				}
+				((DefaultOAuth2AccessToken) accessToken).setScope(scopes);
 
-                        if (role.startsWith("ROLE_")) {
-                            scopes.add(role.substring(5).toLowerCase());
-                        } else {
-                            scopes.add(role.toLowerCase());
-                        }
-                    }
-                    ((DefaultOAuth2AccessToken) accessToken).setScope(scopes);
+			}
+			return accessToken;
+		});
+	}
 
-                }
-                return accessToken;
-            }
-        });
-    }
-    @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
-    }
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
 
-    @Configuration
-    public static class BaseClientConfig {
+	@Configuration
+	public static class BaseClientConfig {
 
-        @Bean
-        @ConfigurationProperties(prefix = "security.oauth2.client")
-        public BaseClientDetails oauth2ClientDetails() {
-            return new BaseClientDetails();
-        }
-    }
+		@Bean
+		@ConfigurationProperties(prefix = "security.oauth2.client")
+		public BaseClientDetails oauth2ClientDetails() {
+			return new BaseClientDetails();
+		}
+	}
 }
